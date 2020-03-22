f51205f2e9ec260a7e53fbe1902dbc141d5ff664
Ludo <ludo@neuviemepage.com>
2020-03-22 21:22:27 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-card>
+          <v-card-title>
+            Covid
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :loading="!loaded"
+            :loading-text="'Fetching data, please wait...'"
+            :headers="headers"
+            :items="items"
+            :items-per-page="25"
+            :search="search"
+          ></v-data-table>
+        </v-card>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      search: "",
+      loaded: false,
+      headers: [
+        {
+          text: "Country",
+          value: "country"
+        },
+        {
+          text: "Cases",
+          value: "cases"
+        },
+        {
+          text: "Today Cases",
+          value: "todayCases"
+        },
+        {
+          text: "Deaths",
+          value: "deaths"
+        },
+        {
+          text: "Today Deaths",
+          value: "todayDeaths"
+        },
+        {
+          text: "Recovered",
+          value: "recovered"
+        },
+        {
+          text: "Critical",
+          value: "critical"
+        }
+      ],
+      items: []
+    };
+  },
+  async mounted() {
+    const { data } = await this.axios.get(
+      "https://corona.lmao.ninja/countries/"
+    );
+    console.log(data);
+    this.items = data;
+    this.loaded=true
+  }
+};
+</script>
